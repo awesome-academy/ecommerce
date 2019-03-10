d57f076062d0273b9f9ae46c41c3520964bfd5e8
tranngochoi <ngochoi1607@gmail.com>
2019-03-10 16:36:05 +0700

    init model and relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Models\Suggest;
+use App\Models\Transaction;
+use App\Models\Contact;
+use App\Models\Comment;
+use App\Models\Role;
 
 class User extends Authenticatable
 {
@@ -39,4 +44,26 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function suggests()
+    {
+        return $this->hasMany(App::Suggest);
+    }
+    public function transactions()
+    {
+        return $this->hasMany(App::Transaction);
+    }
+    public function contacts()
+    {
+        return $this->hasMany(App::Contact);
+    }
+    public function comments()
+    {
+        return $this->hasMany(App::Comment);
+    }
+    public function role()
+    {
+        return $this->belongsTo(App::Role);
+    }
+
 }
