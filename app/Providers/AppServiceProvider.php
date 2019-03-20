5210abf2d5f69f2e8b452b202956896b76f7e705
itlongtruong <truonglong12a1@gmail.com>
2019-03-20 12:43:48 +0700

    add cart

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use App\Models\Cart;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+
+        view()->composer(['public/page/cart', 'public/page/shop-cart'], function ($view) {
+            if (Session('cart')) {
+                $oldCart = Session::get('cart');
+                $cart = new Cart($oldCart);
+                $view->with(['cart' => Session::get('cart'), 'product_cart' => $cart->items, 'totalPrice' => $cart->totalPrice, 'totalQty' => $cart->totalQty]);
+            }
+        });
+
     }
 }
--- resources/views/public/page/shop-cart.blade.php
+++ resources/views/public/page/shop-cart.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div>
     <div>
-        <section class="how-overlay2 bg-img1" style="background-image: url(images/post.jpg);">
+        <section class="how-overlay2 bg-img1" style="background-image: url(/source/images/post.jpg);">
             <div class="container">
                 <div class="txt-center p-t-160 p-b-165">
                     <h2 class="txt-l-101 cl0 txt-center p-b-14 respon1">
@@ -32,31 +32,34 @@
                                 <th class="column-3">Số lượng</th>
                                 <th class="column-4">Tiền</th>
                             </tr>
+
+                            @if(Session::has('cart'))
+                            @foreach($product_cart as $product)
                             <tr class="table_row">
                                 <td class="column-1">
                                     <div class="flex-w flex-m">
                                         <div class="wrap-pic-w size-w-50 bo-all-1 bocl12 m-r-30">
-                                            <img src="images/best-sell-01.jpg" alt="IMG">
+                                            <img src="{{$product['item']['image_list']}}" alt="IMG">
                                         </div>
                                         <span>
-                                        Cheery
+                                        {{$product['item']['name']}}
                                         </span>
                                     </div>
                                 </td>
                                 <td class="column-2">
-                                    18000vnd
+                                    {{number_format($product['item']['price'])}} USD
                                 </td>
                                 <td class="column-3">
                                     <div class="wrap-num-product flex-w flex-m bg12 p-rl-10">
                                         <div class="btn-num-product-down flex-c-m fs-29"></div>
-                                        <input class="txt-m-102 cl6 txt-center num-product" type="number" name="num-product1" value="2">
+                                        <input class="txt-m-102 cl6 txt-center num-product" type="number" name="num-product1" value="{{$product['qty']}}">
                                         <div class="btn-num-product-up flex-c-m fs-16"></div>
                                     </div>
                                 </td>
                                 <td class="column-4">
                                     <div class="flex-w flex-sb-m">
                                         <span>
-                                        36000vnd
+                                            {{number_format($product['item']['price']*$product['qty'])}} USD
                                         </span>
                                         <div class="fs-15 hov-cl10 pointer">
                                             <span class="lnr lnr-cross"></span>
@@ -64,6 +67,8 @@
                                     </div>
                                 </td>
                             </tr>
+                            @endforeach
+                            @endif
                         </table>
                     </div>
                     <div class="flex-w flex-sb-m p-t-20">
@@ -83,15 +88,7 @@
                             Tổng tiền
                             </span>
                             <span class="size-w-59 txt-m-104 cl6">
-                            48000vnd
-                            </span>
-                        </div>
-                        <div class="flex-w flex-m bo-b-1 bocl15 w-full p-tb-18">
-                            <span class="size-w-58 txt-m-109 cl3">
-                            Thành tiền
-                            </span>
-                            <span class="size-w-59 txt-m-104 cl10">
-                            48000vnd
+                                @if(Session::has('cart')){{number_format($totalPrice)}} @else 0 @endif USD
                             </span>
                         </div>
                         <button class="flex-c-m txt-s-105 cl0 bg10 size-a-34 hov-btn2 trans-04 p-rl-10 m-t-43">
